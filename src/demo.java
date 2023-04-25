b8118071484ae9dac0e958654de380d59f9fdab4
MalshaniBandaranayake <malshanibandaranayake91@gmail.com>
2023-04-25 10:27:56 +0530

    java loop codes

---
+++ src/demo.java
@@ -0,0 +1,10 @@
+public class demo {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 8) {
+            System.out.println("hello world!!!");
+            i++;
+        }
+
+    }
+}
